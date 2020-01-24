d320b4332ff06ef238229c1f4b928df671eb5e87
debbysa <deby.silvia23@gmail.com>
2020-01-24 17:08:29 +0700

    koneksi database ads.ajar mysql

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize("ads.ajar", "root", "", {
+  host: "localhost",
+  dialect: "mysql"
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log("koneksi berhasil ke db ads.ajar");
+  })
+  .catch(err => {
+    console.log("error : ", err);
+  });
+module.exports = sequelize;
